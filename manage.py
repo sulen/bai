cf482771b04c3ff8356146414f532599c7e1950f
Stanisław Czech <stanislaw.a.czech@gmail.com>
2024-04-20 16:52:52 +0200

    owasp 6

--- manage.py
+++ manage.py
@@ -2,10 +2,22 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from subprocess import run, PIPE
 
+def run_safety_check():
+    """Run safety check."""
+    result = run(['safety', 'check', '--full-report'], stdout=PIPE, stderr=PIPE)
+    if result.returncode != 0:
+        print("Safety check failed. Exiting.")
+        print(result.stdout.decode('utf-8', errors='ignore'))
+        print(result.stderr.decode('utf-8', errors='ignore'))
+        sys.exit(1)
 
 def main():
     """Run administrative tasks."""
+    # owasp6
+    # run_safety_check()
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bai.settings')
     try:
         from django.core.management import execute_from_command_line
